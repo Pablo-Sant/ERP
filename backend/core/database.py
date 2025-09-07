2738f1caf7e4bb4b3a735de9baeedf106a9c8e13
Pablo <pgssdesouza@gmail.com>
2025-09-07 15:58:40 -0300

    Configuração do arquivo database

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,7 +1,20 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_PASS = os.getenv("DB_PASS")
-DB_USER = os.getenv("DB_USER")
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.configs import settings   
+
+engine:AsyncEngine = create_async_engine(settings.DB_URL)
+
+Session = AsyncSession = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    class_=AsyncSession,
+    bind=engine
+)
+
+
+
