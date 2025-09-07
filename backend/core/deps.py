77211e0b0345afafeb34f10994818bd01e97f53e
Pablo <pgssdesouza@gmail.com>
2025-09-07 16:12:02 -0300

    Configuração do arquivo deps no módulo core

---
+++ backend/core/deps.py
@@ -0,0 +1,12 @@
+# Juntar todas as depedências
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.database import Session
+
+async def get_session():
+    session:AsyncSession = Session()
+
+    try:
+        yield session
+    finally:
+        await session.close()
